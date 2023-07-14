52f0d0d1a1b402d77ba55d716a382e71362d441c
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2023-07-14 17:20:50 +0700

    first commit

---
+++ minitestW2/src/bai1/ManageBook.java
@@ -0,0 +1,10 @@
+package bai1;
+
+public class ManageBook {
+    Book book1 = new FictionBook(1,"Naruto",35000,"Kishimoto Masashi","Viễn tưởng 1");
+    Book book2 = new FictionBook(2,"Conan",25000,"Gosho Aoyama", "Viễn tưởng 2");
+    Book book3 = new FictionBook(3,"Bố già", 45000,"Mario Puzo","Viễn tưởng 1");
+    Book book4 = new FictionBook(4,"7 Viên ngọc rồng",55000,"Paulo Coelho","Viễn tưởng 1");
+    Book book5 = new FictionBook(5,"Đắc Nhân Tâm",85000,"Dale Carnegie","Viễn tưởng 2");
+    
+}
